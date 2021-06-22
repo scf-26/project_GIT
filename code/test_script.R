b7f9315e99c9f179b98840216d6039b8faad35f3
Sadé Ford <sadeford@Sades-MacBook-Air.local>
2021-06-22 12:53:06 -0400

    notes about upregulated and downregulated genes

--- code/test_script.R
+++ code/test_script.R
@@ -13,3 +13,10 @@ dim(iris)
 
 #commmit check
  main
+
+ #downregulated is = low rate of change(less receptors)
+ #upregulated is = high rate of change(more receptors)
+ #In the biological context of organisms' production of gene products
+ #downregulation is the process by which a cell decreases the quantity of a cellular component,
+ #such as RNA or protein, in response to an external stimulus. 
+ #The complementary process that involves increases of such components is called upregulation.
